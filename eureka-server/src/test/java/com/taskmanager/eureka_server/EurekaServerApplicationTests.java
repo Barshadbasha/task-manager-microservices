8552fe99c58dde9b889f5bb420711f22770d0eb9
Barshad Basha <barshadbasha17@gmail.com>
2025-12-11 03:23:56 -0800

    Added Eureka Server and Config Server Spring Boot projects

---
+++ eureka-server/src/test/java/com/taskmanager/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
